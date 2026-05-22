814660548b408c79ec6dad113d59dae9fdc4e2af
lois <lois@lguplus.co.kr>
2026-05-22 22:38:13 +0900

    reduce desktop top spacing

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -23,7 +23,7 @@ interface RenderComponents {
 }
 
 const headerRegex = new RegExp(/h[1-6]/)
-const assetVersion = "20260522-desktop-menu-flow"
+const assetVersion = "20260522-desktop-top-spacing"
 export function pageResources(
   baseDir: FullSlug | RelativeURL,
   staticResources: StaticResources,
