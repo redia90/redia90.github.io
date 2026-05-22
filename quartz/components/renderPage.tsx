4b2e89dd16839f1d26693e99f190f80172b18b9c
lois <lois@lguplus.co.kr>
2026-05-22 22:17:16 +0900

    fix: let desktop left menu flow naturally

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -23,7 +23,7 @@ interface RenderComponents {
 }
 
 const headerRegex = new RegExp(/h[1-6]/)
-const assetVersion = "20260522-menu-under-header"
+const assetVersion = "20260522-desktop-menu-flow"
 export function pageResources(
   baseDir: FullSlug | RelativeURL,
   staticResources: StaticResources,
