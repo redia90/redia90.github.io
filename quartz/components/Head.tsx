2433068e3150d7b4fdbfff106309453f2b17c31d
lois <lois@lguplus.co.kr>
2026-05-22 23:07:29 +0900

    apply pretendard site font

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -50,6 +50,11 @@ export default (() => {
             )}
           </>
         )}
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
