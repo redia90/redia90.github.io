70faabf85828984cfda66855823dd16efc17f0e7
lois <lois@lguplus.co.kr>
2026-05-22 21:42:53 +0900

    fix: keep header visible above mobile menu

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -23,7 +23,7 @@ interface RenderComponents {
 }
 
 const headerRegex = new RegExp(/h[1-6]/)
-const assetVersion = "20260522-menu-spacing"
+const assetVersion = "20260522-menu-under-header"
 export function pageResources(
   baseDir: FullSlug | RelativeURL,
   staticResources: StaticResources,
