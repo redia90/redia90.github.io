0e3d782713b573f31c1e5b324720715108839000
lois <lois@lguplus.co.kr>
2026-05-22 20:39:04 +0900

    style: add spacing to mobile explorer menu

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -23,7 +23,7 @@ interface RenderComponents {
 }
 
 const headerRegex = new RegExp(/h[1-6]/)
-const assetVersion = "20260522-fixed-header-v2"
+const assetVersion = "20260522-menu-spacing"
 export function pageResources(
   baseDir: FullSlug | RelativeURL,
   staticResources: StaticResources,
