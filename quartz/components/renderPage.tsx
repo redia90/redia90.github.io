9bfe860e7502fe8855dab4b1a17267aede82fc85
lois <lois@lguplus.co.kr>
2026-05-22 19:52:02 +0900

    style: keep mobile header sticky

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -23,7 +23,7 @@ interface RenderComponents {
 }
 
 const headerRegex = new RegExp(/h[1-6]/)
-const assetVersion = "20260522-logo-compact"
+const assetVersion = "20260522-sticky-header"
 export function pageResources(
   baseDir: FullSlug | RelativeURL,
   staticResources: StaticResources,
